606a0fc15f2feecd24ab918b8b9836644651fc1a
weizhanzhan <375786117@qq.com>
2020-09-04 09:47:50 +0800

    解决vant打包样式不生效问题

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,7 @@ function resolve(dir) {
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const autoprefixer = require("autoprefixer");
 module.exports = {
+  parallel: false,
   css: {
     loaderOptions: {
       postcss: {

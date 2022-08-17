fd47f33fae1283feb8deb7d10343d3d04fee6e64
weizhanzhan <375786117@qq.com>
2022-08-17 11:29:34 +0800

    video

--- src/views/video/index.tsx
+++ src/views/video/index.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { defineComponent, reactive } from "vue";
 import "./index.less";
 import { useVideoCurrent } from "@/mock/video";
 export default defineComponent({
   setup() {
     const state = reactive({});
-
+    const video = require("@/assets/video/301250279.mp4");
     const getData = () => {
       useVideoCurrent().then((res: Array<{ key: number }>) => {
         console.log(res.map(item => item.key));
@@ -12,14 +13,11 @@ export default defineComponent({
     };
     return () => (
       <div class="videoBox">
-        <button onClick={getData}>获取数据</button>
+        {/* <button onClick={getData}>获取数据</button> */}
         <div class="swipeBox">
           <van-swipe vertical class="swiperDom" show-indicators={false}>
-            <van-swipe-item>
-              <video
-                class="video-item"
-                src="//player.alicdn.com/video/editor.mp4"
-              ></video>
+            <van-swipe-item class="swipe-item">
+              <video class="video-item" autoplay="true" src={video}></video>
             </van-swipe-item>
             <van-swipe-item>2</van-swipe-item>
             <van-swipe-item>3</van-swipe-item>
